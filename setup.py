3984ccee85bffe7a6699e726e4718ec845d582e9
danmaclean <maclean.daniel@gmail.com>
2019-09-16 12:23:32 +0100

    refactor

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup
 
 setup(
-    name='redspot',
+    name='redpatch',
     version='0.0.1dev0',
-    packages=['redspot', 'redspot_notebooks'],
+    packages=['redpatch', 'redpatch_notebooks'],
     url='https://github.com/TeamMacLean/redspot',
     license='LICENSE.txt',
     author='Dan MacLean',
     author_email='dan.maclean@tsl.ac.uk',
     description='Finding Disease Lesions in Plant Leaves',
-    scripts=['scripts/redspot-start'],
+    scripts=['scripts/redpatch-start'],
     include_package_data=True,
-    package_data={"redspot_notebooks": ['Redspot Basic Use Example.ipynb']},
+    package_data={"redpatch_notebooks": ['Redpatch Basic Use Example.ipynb']},
     python_requires='>=3.7',
     install_requires=[
         "jupyter >= 1.0.0",
