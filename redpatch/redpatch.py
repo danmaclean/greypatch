648123b2772fe39fd8c3ae9ef0510dae8bdc5c89
danmaclean <maclean.daniel@gmail.com>
2019-09-30 15:46:33 +0100

    add threshold via sliders

--- redpatch/redpatch.py
+++ redpatch/redpatch.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from typing import Callable, List, Tuple, Union
 from ipywidgets import FloatRangeSlider, FloatProgress
+import ipywidgets as widgets
 
 
 LEAF_AREA_HUE = tuple([i / 255 for i in (0, 255)])
