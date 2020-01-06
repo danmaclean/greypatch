8ba12a5590daef565341b1aa55e1fc8afca2bcb2
danmaclean <maclean.daniel@gmail.com>
2020-01-06 16:56:34 +0000

    basic info in batch process

--- scripts/batch_process.py
+++ scripts/batch_process.py
@@ -1,8 +1,13 @@
 import redpatch as rp
+import filtersettings as filters
 
-fs = FilterSettings()
-fs.add_setting("leaf_area", h=rp.LEAF_AREA_HUE, s=rp.LEAF_AREA_SAT, v=rp.LEAF_AREA_VAL)
-fs.write("test.yml")
+
+def create_default_filter_file(file="default_filter.yml"):
+    fs = filters.FilterSettings()
+    fs.add_setting("leaf_area", h=rp.LEAF_AREA_HUE, s=rp.LEAF_AREA_SAT, v=rp.LEAF_AREA_VAL)
+    fs.add_setting("healthy_area", h=rp.HEALTHY_HUE, s=rp.HEALTHY_SAT, v=rp.HEALTHY_VAL)
+    fs.add_setting("lesion_area", h=rp.LESION_HUE, s=rp.LESION_SAT, v=rp.LESION_VAL)
+    fs.write(file)
 
 import numpy as np
 from skimage import measure
@@ -29,31 +34,32 @@ def _get_sub_images(imfile, fs):
     props = get_object_properties(three_d_final_labelled_leaf_area, intensity_image=im)
     sub_labels = [p.image.astype(int)[:, :, -1] for p in props]
     sub_images = [p.intensity_image for p in props]
-    cleared_leaf_sub_image = [rp.clear_background(sub_images[i], sub_labels[i]) for i in range(len(sub_labels))]
+    cleared_leaf_sub_images = [rp.clear_background(sub_images[i], sub_labels[i]) for i in range(len(sub_labels))]
     return (cleared_leaf_sub_images, sub_labels)
 
 
-def _get_healthy_portion(im, fs):
+def _get_healthy_areas(im, fs):
     healthy_mask, healthy_volume = rp.griffin_healthy_regions(im,
                                                               h=fs['healthy_area']['h'],
                                                               s=fs['healthy_area']['s'],
                                                               v=fs['healthy_area']['v'])
-    ##here add all code to get a list of lesion regions object - ie labelled, filtered and properties found
-    ##
-    return healthy_mask
+    labelled_healthy_area, healthy_areas_found = rp.label_image(healthy_mask)
+    labelled_healthy_area_properties = rp.get_object_properties(labelled_healthy_area)
+    return labelled_healthy_area_properties
 
 
-def _get_lesion_masks(im, fs):
-    lesion_region_mask, lesion_region_volume = rp.griffin_lesion_regions(im,
-                                                                         h=fs['lesion_area']['h'],
-                                                                         s=fs['lesion_area']['s'],
-                                                                         v=fs['lesion_area']['v'])
-    ##here add all code to get a list of lesion regions object - ie labelled, filtered and properties found
-    ##
-    return lesion_region_mask
+def _get_lesion_areas(im, fs):
+    lesion_area_mask, lesion_region_volume = rp.griffin_lesion_regions(im,
+                                                                       h=fs['lesion_area']['h'],
+                                                                       s=fs['lesion_area']['s'],
+                                                                       v=fs['lesion_area']['v'])
+    labelled_lesion_area, lesion_areas_found = rp.label_image(lesion_area_mask)
+    labelled_lesion_area_properties = rp.get_object_properties(labelled_lesion_area)
+    return labelled_lesion_area_properties
 
 
-def _get_lesion_centres(???)
+def _get_lesion_centres():
+    pass
 
 
 import os
@@ -62,19 +68,19 @@ import importlib
 importlib.reload(rp)
 
 
-def batch_process(folder=".", settings="test.yml"):
-    fs = FilterSettings()
+def batch_process(folder=".", settings="settings.yml"):
+    fs = filters.FilterSettings()
     fs.read(settings)
     image_files = os.listdir(folder)
     for imfile in image_files:
-        ims, labels = _get_sub_images(imfile, fs)
-        for sub_im in ims:
-            healthy_masks = [_get_healthy_masks(s, fs) for i in ims]  # 0 or 1 per image
-            filtered_healthy_masks =
-            lesion_objs = [_get_lesion_masks(i, fs) for i in ims]  # 0 to many per image
-            for lr in lesion_masks:
-                lesion_centres = _get_lesion_centres(lr)
-        return ims, labels
+        sub_ims, labels = _get_sub_images(imfile, fs)
+        for sub_i in sub_ims:
+            healthy_obj_props = _get_healthy_areas(sub_i, fs)
+            lesion_area_props = _get_lesion_areas(sub_i, fs)  # 0 to many per image
+            # lesion_centre_props = _get_lesion_centres(sub_i,fs)
+            #now report -
+# TODO tidy table, pandas summary of table, annotated image, make runnable as script.
 
 
-ims, labels = batch_process("/Users/macleand/Desktop/single_image/")
+
+batch_process("/Users/macleand/Desktop/single_image/")
