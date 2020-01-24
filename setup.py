1d84d17d1319f859ee0265e24b8516be526968cb
danmaclean <maclean.daniel@gmail.com>
2020-01-24 15:25:18 +0000

    add readme

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='redpatch',
-    version='0.0.1.dev25',
+    version='0.0.1.dev26',
     packages=['redpatch', 'redpatch_notebooks'],
     url='https://github.com/TeamMacLean/redspot',
     license='LICENSE.txt',
