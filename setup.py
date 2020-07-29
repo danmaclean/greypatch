e6aa68170930326534bf5561a8005f86dfc54a45
danmaclean <maclean.daniel@gmail.com>
2020-07-29 16:21:40 +0100

    bump version pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='redpatch',
-    version='0.1.0',
+    version='0.1.1',
     packages=['redpatch', 'redpatch_notebooks'],
     url='https://github.com/TeamMacLean/redpatch',
     license='LICENSE.txt',
